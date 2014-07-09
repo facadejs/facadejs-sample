a8ec24252e8b8ef9233d9b4c7e7e8b9a2ac8cbeb
Scott Doxey <neogeek0101@gmail.com>
2014-07-08 23:40:36 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+#Facade.js Sample Project
+
+##Install
+
+```bash
+$ npm install
+```
+
+##Run
+
+If you have the [Heroku Toolbelt](https://toolbelt.heroku.com/) installed you can start the sample applications with this command:
+
+```bash
+$ foreman start
+```
+
+If not, then the server can also be run using this command:
+
+```bash
+$ node web.js
+```
---
+++ static/js/app.js
@@ -0,0 +1,68 @@
+/*jslint browser: true */
+/*globals Facade, Gamepad, $ */
+
+(function () {
+
+    'use strict';
+
+    var stage = new Facade(document.querySelector('canvas')),
+        controls = new Gamepad(),
+        world = new Facade.Entity().Box2D('createWorld', { canvas: stage.canvas, gravity: [ 0, 20 ] }),
+        objects = {
+            level: [],
+            player: null
+        };
+
+    function generateEntityFromObject(data) {
+
+        var entity;
+
+        entity = new Facade.Rect(data.options);
+
+        entity.Box2D('createObject', world, data.box2d_properties);
+
+        return entity;
+
+    }
+
+    $.getJSON('data/sample.json', function (data) {
+
+        objects.level = data.level.map(generateEntityFromObject);
+        objects.player = generateEntityFromObject(data.player);
+
+    });
+
+    stage.resizeForHDPI();
+
+    stage.draw(function () {
+
+        this.clear();
+
+        world.Box2D('step');
+
+        this.addToStage(objects.level);
+        this.addToStage(objects.player);
+
+        // world.Box2D('drawDebug');
+
+    });
+
+    controls.on('press', 'button_1', function () {
+
+        objects.player.Box2D('setVelocity', objects.player.Box2D('getVelocity').x, -15);
+
+    });
+
+    controls.on('hold', 'd_pad_left', function () {
+
+        objects.player.Box2D('setVelocity', -5, objects.player.Box2D('getVelocity').y);
+
+    });
+
+    controls.on('hold', 'd_pad_right', function () {
+
+        objects.player.Box2D('setVelocity', 5, objects.player.Box2D('getVelocity').y);
+
+    });
+
+}());
