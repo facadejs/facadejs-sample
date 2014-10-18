81d80a4fc7c6da2884732a2b938969bde2699014
Scott Doxey <neogeek0101@gmail.com>
2014-10-18 02:09:44 -0400

    Reordered files.

--- static/js/config.js
+++ static/js/config.js
@@ -1,10 +1,10 @@
 require.config({
     'paths': {
-        'jquery': '../bower_components/jquery/dist/jquery.min',
-        'facade': '../bower_components/facade.js/facade.min',
-        'gamepadjs': '../bower_components/gamepad.js/gamepad.min',
         'box2dweb': '../bower_components/facadejs-Box2D-plugin/vendor/box2dweb/Box2dWeb-2.1.a.3',
-        'facadejs-Box2D-plugin': '../bower_components/facadejs-Box2D-plugin/facadejs-Box2D'
+        'facade': '../bower_components/facade.js/facade.min',
+        'facadejs-Box2D-plugin': '../bower_components/facadejs-Box2D-plugin/facadejs-Box2D',
+        'gamepadjs': '../bower_components/gamepad.js/gamepad.min',
+        'jquery': '../bower_components/jquery/dist/jquery.min'
     },
     'shim': {
         'box2dweb': {
